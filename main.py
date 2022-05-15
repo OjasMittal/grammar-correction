08c9f68898b792869a0882af0815f1c69a28b002
Ojas Mittal <ojasfarm31@gmail.com>
2022-05-15 18:15:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import json
+url='https://api.languagetool.org/v2/check'
+data={
+  'text':'Tis is a nix day!',
+  'language':'auto'
+}
+response=requests.post(url,data=data)
+result=json.loads(response.text)
+print(result)
